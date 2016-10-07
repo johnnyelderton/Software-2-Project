ed8b95371c3fdd9474889491aca1897cd9e6d947
Ama Ott <amaott13@gmail.com>
2016-10-07 14:44:53 -0500

    Added underwater visual effects

---
+++ Assets/Scripts/Swimmer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Swimmer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        RenderSettings.fog = false;
+        RenderSettings.fogColor = new Color(0.2f, 0.4f, 0.8f, 0.5f);
+        RenderSettings.fogDensity = 0.09f;
+	}
+	
+    bool isUnderWater()
+    {
+        return gameObject.transform.position.y < 4.5f;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        RenderSettings.fog = isUnderWater();
+	}
+}

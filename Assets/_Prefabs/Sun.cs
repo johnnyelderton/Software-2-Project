3b051ca77ae67d585d77764204dc13ba68f9838d
Oswald Yao <koffi.yao@ttu.edu>
2016-10-09 12:43:33 -0500

    Modified terrain, added colliders, and added a day/night cycle.

---
+++ Assets/_Prefabs/Sun.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Sun : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(Vector3.zero, Vector3.right, 1f * Time.deltaTime);
+        transform.LookAt(Vector3.zero);
+	}
+}

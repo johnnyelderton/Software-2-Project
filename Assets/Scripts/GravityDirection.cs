737dc1890a6ca98536a9995714b1f50a0e22a3f1
csprofessionalcarl <csprofessionalcarl@gmail.com>
2016-10-13 23:52:08 -0500

    Created Gravity Direction Change Base Script, Press G to initiate direction change. Make sure script is a component of camera

---
+++ Assets/Scripts/GravityDirection.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityDirection : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GravChange();
+	}
+
+    private void GravChange()
+    {
+        if(Input.GetKey (KeyCode.G))
+        {
+
+            Physics.gravity = GetComponent<Camera>().transform.forward;
+        }
+    }
+}

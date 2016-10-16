3edb66cc9a1c1cb40d7998d8ea3400203f643deb
Carl Carter <csprofessionalcarl@gmail.com>
2016-10-16 14:31:28 -0500

    Changed Aesthitics of Gravity Temple Including: Skybox, Maze Texture, Lighting, Design. Pushing In Progress Faux Attractor Script as well to circumvent errors

---
+++ Assets/Scripts/FauxTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FauxTrigger : MonoBehaviour {
+	GameObject Planet;
+
+	// Use this for initialization
+	void Start () {
+		Planet.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter(Collider coll){
+		if(coll.gameObject.tag == "Player")
+			Planet.SetActive (true);
+	}
+
+	private void OnTriggerExit(){
+		Planet.SetActive (false);
+	}
+
+
+}

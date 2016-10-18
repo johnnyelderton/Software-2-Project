e501e900bb8b79994d4dcaf164b3b50d2b747c77
Oswald Yao <koffi.yao@ttu.edu>
2016-10-18 12:28:45 -0500

    Added a temporary trigger to go in the Water Temple from the main Terrain

---
+++ Assets/Scripts/cube_script.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class cube_script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    void OnTriggerEnter()
+    {
+        // Go to water temple
+        SceneManager.LoadScene("Water Temple");
+    }
+}

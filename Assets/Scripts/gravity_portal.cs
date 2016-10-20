b55ec26b6208bd85ac5579ade2b861cef47e7e21
Oswald Yao <koffi.yao@ttu.edu>
2016-10-19 20:22:37 -0500

    Added a "portal" to go from the main terrain to the Gravity Temple.

---
+++ Assets/Scripts/gravity_portal.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class gravity_portal : MonoBehaviour
+{
+
+    void OnTriggerEnter()
+    {
+        // Go to gravity temple
+        SceneManager.LoadScene("Gravity Temp");
+    }
+}

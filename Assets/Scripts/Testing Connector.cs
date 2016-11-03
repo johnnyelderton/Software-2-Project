758c53dd238a2490bb8e806096deb814341ce5da
Johnny <johnnymustang7@gmail.com>
2016-11-03 14:55:31 -0500

    This is just a test connector to see how to connect to a sever. But for right now it is connecting to the localhost but everyones port is different so it is commented out for now.

---
+++ Assets/Scripts/Testing Connector.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System; 
+
+public class TestingConnector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Network.Connect ("127.0.0.1", 3306); 
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

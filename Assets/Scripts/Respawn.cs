476b7f569ba80f746b712ec07aba97e0030cf4e1
Carl Carter <csprofessionalcarl@gmail.com>
2016-11-13 15:48:56 -0600

    Created Respawn/Teleport Script for when player falls out of bounds, or needs to teleport. Added respawn deadzone to Light Temple

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Respawn : MonoBehaviour {
+
+	//Game Object you want player to teleport to
+	public GameObject respawn;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void OnTriggerEnter(Collider coll)
+	{
+		//FirstPersonController needs to have "Player" as Tag
+		if (coll.gameObject.tag == "Player")
+		{
+			//Changes player position to GameObject
+			coll.transform.position = respawn.transform.position;
+		}
+	}
+
+}

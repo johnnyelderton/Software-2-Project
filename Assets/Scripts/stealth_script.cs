5639078b7a450a6e25628df63c641ed0c4060d26
Oswald Yao <koffi.yao@ttu.edu>
2016-11-14 12:25:07 -0600

    Modified the terrain as well as colliders and added a portal for Stealth Temple.

---
+++ Assets/Scripts/stealth_script.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class stealth_script : MonoBehaviour {
+
+    IEnumerator OnTriggerEnter()
+    {
+        // Go to water temple
+        float fadeTime = GameObject.Find("ChangeLevel").GetComponent<Fading>().BeginFade(1);
+        yield return new WaitForSeconds(fadeTime);
+        SceneManager.LoadScene("Stealth Temple");
+    }
+}

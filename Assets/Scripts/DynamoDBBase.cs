fb8e75eb9f71a798cf0fd4603c64fe3932f77a54
Johnny <johnnymustang7@gmail.com>
2016-11-15 02:50:32 -0600

    Added functionality to the login button from the login menu, but errors are still being created. And added the base for the database.

--- Assets/Scripts/DynamoDBBase.cs
+++ Assets/Scripts/DynamoDBBase.cs
@@ -44,7 +44,6 @@ namespace AssemblyCSharp
 				return _ddbClient; 
 			}
 		}
-
 	}
 }
 
--- Assets/Scripts/LoginToDB.cs
+++ Assets/Scripts/LoginToDB.cs
@@ -1,28 +1,80 @@
 ﻿using UnityEngine;
 using System.Collections;
-using AWSSDK;
+using UnityEngine.UI;
+using Amazon.DynamoDBv2.DocumentModel;
+using System;
+using System.Threading;
 using Amazon;
-using Amazon.Auth;
-using Amazon.CognitoIdentity;
 using Amazon.DynamoDBv2;
-using Amazon.Internal;
-using Amazon.MissingTypes;
-using Amazon.Runtime;
-using Amazon.SecurityToken;
-using Amazon.Util; 
+using Amazon.Runtime.Internal;
+using System.Collections.Generic;
+using Amazon.Util;
+using Amazon.DynamoDBv2.Model;
+using Amazon.DynamoDBv2.DataModel;
 
+namespace Scripts {
+	public class LoginToDB : MonoBehaviour {
+		// initializing variables for strign and game objects
+		public Button login; 
+		public string username;
+		public string password;
+		public int userID; 
+		public bool answer; 
+		public Text resultText; 
+		public GameObject InputFieldUsername; 
+		public GameObject InputFieldPassword;
+		InputField inputUsername; 
+		InputField inputPassword;
 
-public class LoginToDB : MonoBehaviour {
+		// initializing a tabel players
+		private static Table playersTable; 
 
-	// Use this for initialization
-	void Start () {
-		//UnityInitializer.AttachToGameObject (this.gameObject); 
-		//var credentials = new CognitoAWSCredentials (IDENTITY_POOL_ID, RegionEndpoint.USEast1);
-		//AmazonDynamoDBClient client = new AmazonDynamoDBClient (AWSCredentials);
-		//DynamoDBContextConfig Context = new DynamoDBContextConfig (client); 
+		// initializing the client
+		private IAmazonDynamoDB _client; 
 
+		// Use this for initialization
+		void Start () {  
+			login.onClick.AddListener (loginListener); 
+		}
 
+		public void loginListener(){
+			inputUsername = InputFieldUsername.GetComponent<InputField> (); 
+			inputPassword = InputFieldPassword.GetComponent<InputField> ();
+			username = inputUsername.text; 
+			password = inputPassword.text;  
+			loadPlayersTable (); 
+		}
+
+		public void loadPlayersTable(){
+			//resultText.text = "\n***LoadTable***";
+			Table.LoadTableAsync (_client, "Players", (loadTableResults) => {
+				if (loadTableResults.Exception != null) {
+					resultText.text += "\n Failed to load Players table"; 
+				} else {
+					playersTable = loadTableResults.Result;
+					GetPlayersTable(); 
+				}
+			});
+		}
+
+		public static void GetPlayersTable(){
+			
+		}
+
+		public bool checkLogin(string user, string pass){
+			bool check = true; 
+
+			return check; 
+		}
+
+		[DynamoDBTable("Players")]
+		public class players {
+			[DynamoDBHashKey]
+			public int ID{ get; set; }
+			[DynamoDBProperty]
+			public string username{ get; set; }
+			[DynamoDBProperty]
+			public string password{ get; set; }
+		}
 	}
-
-
 }

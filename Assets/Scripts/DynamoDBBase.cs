a766f79fddf257aba836d43d66a2c37b33760e94
Johnny <johnnymustang7@gmail.com>
2016-11-14 23:51:12 -0600

    Added new files for the constructor for the database and for the AWS configuration xml file.

---
+++ Assets/Scripts/DynamoDBBase.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using Amazon.DynamoDBv2;
+using Amazon.CognitoIdentity;
+using Amazon.Runtime;
+using Amazon;
+
+namespace AssemblyCSharp
+{
+	public class DynamoDBBase : MonoBehaviour
+	{
+		public string IdentityPoolID = "";
+		public string CognitoPoolRegion = RegionEndpoint.USEast1.SystemName;
+		public string DynamoRegion = RegionEndpoint.USEast1.SystemName;
+		private static IAmazonDynamoDB _ddbClient; 
+		private AWSCredentials _credentials; 
+
+		private RegionEndpoint _CongnitoPoolRegion{
+			get{
+				return RegionEndpoint.GetBySystemName (CognitoPoolRegion); 
+			}
+		}
+
+		private RegionEndpoint _DynamoRegion{
+			get{
+				return RegionEndpoint.GetBySystemName (DynamoRegion); 
+			}
+		}
+
+		private AWSCredentials credentials{
+			get{
+				if (_credentials == null) {
+					_credentials = new CognitoAWSCredentials (IdentityPoolID, _CongnitoPoolRegion);
+				}
+				return _credentials; 
+			}
+		}
+
+		protected IAmazonDynamoDB Client{
+			get{
+				if (_ddbClient == null) {
+					_ddbClient = new AmazonDynamoDBClient (credentials, _DynamoRegion); 
+				}
+				return _ddbClient; 
+			}
+		}
+
+	}
+}
+
---
+++ Assets/Scripts/LoginToDB.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using AWSSDK;
+using Amazon;
+using Amazon.Auth;
+using Amazon.CognitoIdentity;
+using Amazon.DynamoDBv2;
+using Amazon.Internal;
+using Amazon.MissingTypes;
+using Amazon.Runtime;
+using Amazon.SecurityToken;
+using Amazon.Util; 
+
+
+public class LoginToDB : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//UnityInitializer.AttachToGameObject (this.gameObject); 
+		//var credentials = new CognitoAWSCredentials (IDENTITY_POOL_ID, RegionEndpoint.USEast1);
+		//AmazonDynamoDBClient client = new AmazonDynamoDBClient (AWSCredentials);
+		//DynamoDBContextConfig Context = new DynamoDBContextConfig (client); 
+
+
+	}
+
+
+}

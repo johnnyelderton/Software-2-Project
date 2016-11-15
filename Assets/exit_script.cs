0dbc4b0cd205607ad14a741b464e285083bdf19f
Oswald Yao <koffi.yao@ttu.edu>
2016-11-14 23:24:45 -0600

    Added an exit to the Gravity temple

---
+++ Assets/exit_script.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class exit_script : MonoBehaviour {
+
+    IEnumerator OnTriggerEnter()
+    {
+        // Go to main Terrain
+        float fadeTime = GameObject.Find("ChangeLevel").GetComponent<Fading>().BeginFade(1);
+        yield return new WaitForSeconds(fadeTime);
+        SceneManager.LoadScene("Terrain");
+    }
+}

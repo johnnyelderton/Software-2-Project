6d3acc878df72315f15cf162cf39a7248a8b53c4
Johnny <johnnymustang7@gmail.com>
2016-12-04 10:36:35 -0600

    Added the scripts for saving the player information and for the inventory.

---
+++ Assets/Scripts/PlayerSave.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerSave : MonoBehaviour {
+
+	// Use this for initialization
+	float xLocation;
+	float yLocation;
+	float zLocation;
+	string scene;
+
+	float getXLoaction(){
+		return xLocation;
+	}
+
+	float getYLoaction(){
+		return yLocation;
+	}
+
+	float getZLoaction(){
+		return zLocation;
+	}
+
+	string getScene(){
+		return scene;
+	}
+
+	void sendXLocation(float x){
+		xLocation = x;
+	}
+
+	void sendYLocation(float y){
+		yLocation = y; 
+	}
+
+	void sendZLocation(float z){
+		zLocation = z; 
+	}
+
+	void sendScene(string s){
+		scene = s;
+	}
+}
---
+++ Assets/Scripts/Saving.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using Scripts;  
+
+public class Saving : MonoBehaviour{
+
+	public Button save;
+	public GameObject FPS; 
+	public float x;
+	public float y;
+	public float z;
+	public string scene; 
+
+	public void onSaveClick (){
+		  
+	}
+}
+

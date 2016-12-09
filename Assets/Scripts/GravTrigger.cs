4fa17b9efb324bb2acbdc08e5803a1633f1ef2c2
Carl Carter <csprofessionalcarl@gmail.com>
2016-12-08 22:35:13 -0600

    Restricted Gravity Direction Change to Maze

---
+++ Assets/Scripts/GravTrigger.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravTrigger : MonoBehaviour {
+	private GravityDirection grav;
+
+	// Use this for initialization
+	void Start () {
+		grav = GetComponent<GravityDirection>();
+		grav.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void OnTriggerEnter(Collider coll)
+	{
+		
+		if (coll.gameObject.tag == "GravTrig")
+		{
+			//Changes player position to GameObject
+			grav.enabled = true;
+		}
+	}
+
+	private void OnTriggerExit()
+	{
+		grav.enabled = false;
+		Physics.gravity = new Vector3(0, -9.8F, 0);
+	}
+}

3b4336d15fa1e907297430f9126e7708bf8c6572
Johnny <johnnymustang7@gmail.com>
2016-12-09 01:04:38 -0600

    Added the rotation of the object to saving and to loading but the values are barely altered. So it might be removed in the future.

--- Assets/Scripts/LoadPlayer.cs
+++ Assets/Scripts/LoadPlayer.cs
@@ -10,9 +10,11 @@ public class LoadPlayer : MonoBehaviour {
 	public static GameObject FPS; 
 
 	// initializing values xSaved, ySaved, and zSaved.
-	public static float xSaved;
-	public static float ySaved;
-	public static float zSaved;
+	// and initializing values xRSaved, yRSaved, zRSaved.
+	public static float xSaved;	public static float xRSaved;
+	public static float ySaved;	public static float yRSaved;
+	public static float zSaved;	public static float zRSaved; 
+	public static float wRSaved;
 
 	// initializing the boolean check.
 	public static bool check;
@@ -36,13 +38,23 @@ public class LoadPlayer : MonoBehaviour {
 		// sets FPS to the player controller in each scene. 
 		FPS = GameObject.Find ("FPS"+scene);
 
-		// establishes the x,y, and z values that are saved. 
+		// establishes the x,y, and z values of location that are saved. 
 		xSaved = PlayerSave.getXLocation (); 
 		ySaved = PlayerSave.getYLocation ();
 		zSaved = PlayerSave.getZLocation (); 
 
-		// sets a new vector that is the same as the x,y and z values saved. 
+		// establishes the x, y, z, and w values of rotation that are saved.
+		xRSaved = PlayerSave.getXRotation ();
+		yRSaved = PlayerSave.getYRotation ();
+		zRSaved = PlayerSave.getZRotation ();
+		wRSaved = PlayerSave.getWRotation ();
+
+		// sets a new vector that is the same as the x,y and z values of location saved. 
 		Vector3 saved = new Vector3 (xSaved, ySaved, zSaved);
+		// sets a new quaternion that is the same as the x, y, z, and w values of rotation saved.
+		Quaternion rsaved = new Quaternion (xRSaved,yRSaved, zRSaved, wRSaved);
+
+		//print (rsaved); 
 
 		// makes check equal to if the player has saved the game before.
 		check = PlayerSave.getHasSaved ();
@@ -51,7 +63,9 @@ public class LoadPlayer : MonoBehaviour {
 		// of x,y and z are not 0.
 		if(check == true && (xSaved != 0 && ySaved !=0 && zSaved!=0)){
 			FPS.transform.position = saved;	// makes the orientation of the player object equal to the x,y,
-			// and z values stores in the save.
+			// and z values stored in the save.
+			FPS.transform.rotation = rsaved;  // makes the rotation of the player object equal to the x,
+			// y, z, and w values stored in the save.
 			PlayerSave.sendHasSaved (false); // makes has saved to false in the player save.
 		}
 	}
--- Assets/Scripts/Saving.cs
+++ Assets/Scripts/Saving.cs
@@ -8,20 +8,39 @@ public class Saving : MonoBehaviour{
 
 	public Button save;
 	public GameObject FPS; 
-	public float xLocation;
-	public float yLocation;
-	public float zLocation;
+
+	public float xLocation;	public float xRotation;
+	public float yLocation;	public float yRotation;
+	public float zLocation;	public float zRotation;
+	public float wRotation; 
+
 	public string scene;  
 
 	public void onSaveClick (){
 		xLocation = FPS.transform.position.x;   
 		yLocation = FPS.transform.position.y;
 		zLocation = FPS.transform.position.z;  
+
+		xRotation = FPS.transform.rotation.x;
+		yRotation = FPS.transform.rotation.y;
+		zRotation = FPS.transform.rotation.z;
+		wRotation = FPS.transform.rotation.w;
+		//print (xRotation); 
+		//print (yRotation); 
+		//print (zRotation);
+		//print (wRotation); 
+
 		scene = SceneManager.GetActiveScene ().name; 
 
 		PlayerSave.sendXLocation (xLocation);
 		PlayerSave.sendYLocation (yLocation);
 		PlayerSave.sendZLocation (zLocation);
+
+		PlayerSave.sendXRotation (xRotation);
+		PlayerSave.sendYRotation (yRotation);
+		PlayerSave.sendZRotation (zRotation);
+		PlayerSave.sendWRotation (wRotation); 
+
 		PlayerSave.sendScene (scene); 
 		PlayerSave.sendHasSaved (true); 
 

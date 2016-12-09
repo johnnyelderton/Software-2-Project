0c7f6d38772592e3f8a877eb30a466d699bc4f71
Oswald Yao <koffi.yao@ttu.edu>
2016-12-08 21:44:58 -0600

    Added an exit to the water temple and an entrance to the main temple.

---
+++ Assets/Light_entrance.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Light_entrance : MonoBehaviour {
+
+    IEnumerator OnTriggerEnter()
+    {
+        // Go to light temple
+        float fadeTime = GameObject.Find("ChangeLevel").GetComponent<Fading>().BeginFade(1);
+        yield return new WaitForSeconds(fadeTime);
+        SceneManager.LoadScene("Light Temple");
+    }
+}

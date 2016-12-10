41917139b40090cefb3a5c617fd90c6fd17bded1
Johnny <johnnymustang7@gmail.com>
2016-12-10 00:32:20 -0600

    Added loading inventory to all of the scenes and finished the load inventory script.

--- Assets/Scripts/LoadInventory.cs
+++ Assets/Scripts/LoadInventory.cs
@@ -1,15 +1,61 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class LoadInventory : MonoBehaviour {
 
 	// Use this for initialization
+
+	bool wireBundle; // part in the water temple
+	bool autonomicRT; // part in the gravity temple
+	bool booster;	// part in the light temple
+	bool warpCore; // part in the stealth temple
+	bool check;
+
+	public GameObject partWater;
+	public GameObject partStealth;
+	public GameObject partLight;
+	public GameObject partGravity;
+	public GameObject menu;
+
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		wireBundle = PlayerSave.getWaterPart ();
+		autonomicRT = PlayerSave.getGravityPart ();
+		booster = PlayerSave.getLightPart ();
+		warpCore = PlayerSave.getStealthPart (); 
+		check = menu.activeSelf; 
+
+		if (wireBundle == true) {
+			if (check == true) {
+				partWater.SetActive (true);
+			}
+		}
+
+		if (warpCore == true) {
+			if (check == true) {
+				partStealth.SetActive (true);
+			}
+		}
+
+		if (booster == true) {
+			if (check == true) {
+				partLight.SetActive (true);
+			}
+		}
+
+		if (autonomicRT == true) {
+			if (check == true) {
+				partGravity.SetActive (true);
+			}
+		}
+
+
 	}
+
+
 }

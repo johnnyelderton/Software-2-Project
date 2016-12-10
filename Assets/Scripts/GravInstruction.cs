5bf68a7fbfa710df77ee0a8911334a2947965ac9
Carl Carter <csprofessionalcarl@gmail.com>
2016-12-10 03:21:10 -0600

    Final Push For Presentation

---
+++ Assets/Scripts/GravInstruction.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravInstruction : MonoBehaviour {
+	public GameObject text;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnTriggerEnter(Collider other){
+			text.SetActive (true);
+	}
+
+	public void OnTriggerExit(Collider other){
+			text.SetActive (false);
+	}
+}
